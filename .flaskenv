1fd4ff014c7bcec3dcb6fd9b8ebcc94eaa6860d2
SleepyDude <ko_on@mail.ru>
2022-11-20 20:38:39 +0700

    show test data

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-export FLASK_ENV=development
+export FLASK_DEBUG=True
 export FLASK_APP=src/__init__.py
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,20 @@
 from flask import current_app as app
-from flask import request
+from flask import request, render_template
 
 @app.get("/")
 def show_all():
-    return "Showing all info"
+    headers = [
+        {
+            'name': 'accept',
+            'value': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        },
+        {
+            'name': 'accept-encoding',
+            'value': 'gzip, deflate, br'
+        },
+        {
+            'name': 'accept-language',
+            'value': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
+        }
+    ]
+    return render_template("check_all.html", headers=headers)
