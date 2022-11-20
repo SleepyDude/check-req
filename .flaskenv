373613f89ed8cf35907f465aada9d9c4fa104bc6
SleepyDude <ko_on@mail.ru>
2022-11-20 23:44:23 +0700

    some minor changes

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
 export FLASK_DEBUG=True
-export FLASK_APP=src/__init__.py
+export FLASK_APP=src
--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,9 @@
 from flask import current_app as app
 from flask import request, render_template
-from pprint import pprint as pp
 
 @app.get("/")
 def show_all():
     headers = request.headers
     # pp(hdrs)
     cookies = request.cookies
-    pp(cookies)
     return render_template("check_all.html", headers=headers, cookies=cookies)
