52fe1d017ef6934c7b8e4e79bd46414b729bdcc9
SleepyDude <ko_on@mail.ru>
2022-11-20 21:00:27 +0700

    cookie detect

--- src/routes.py
+++ src/routes.py
@@ -1,20 +1,11 @@
 from flask import current_app as app
 from flask import request, render_template
+from pprint import pprint as pp
 
 @app.get("/")
 def show_all():
-    headers = [
-        {
-            'name': 'accept',
-            'value': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-        },
-        {
-            'name': 'accept-encoding',
-            'value': 'gzip, deflate, br'
-        },
-        {
-            'name': 'accept-language',
-            'value': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
-        }
-    ]
-    return render_template("check_all.html", headers=headers)
+    headers = request.headers
+    # pp(hdrs)
+    cookies = request.cookies
+    pp(cookies)
+    return render_template("check_all.html", headers=headers, cookies=cookies)
