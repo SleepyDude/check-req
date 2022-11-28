0465a55339fefaff3a0bb4cc6f08a88ad83c010d
SleepyDude <ko_on@mail.ru>
2022-11-28 19:58:20 +0700

    ip api

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,7 @@
 from flask import current_app as app
-from flask import request, render_template
+from flask import request, render_template, jsonify
+
+import requests
 
 @app.get("/")
 def show_all():
@@ -7,3 +9,28 @@ def show_all():
     # pp(hdrs)
     cookies = request.cookies
     return render_template("check_all.html", headers=headers, cookies=cookies)
+
+@app.get("/v1/get_my_ip_data")
+def ip_data():
+    ip = request.remote_addr
+    print('ip:', ip)
+    resp = None
+    try:
+        resp = requests.get('http://ip-api.com/json/{}'.format(ip), timeout=3)
+    except Exception as e:
+        print(e)
+
+    country = 'N/A'
+    country_code = 'N/A'
+    if resp and resp.status_code == 200:
+        rjson = resp.json()         
+        print(rjson)
+        if rjson['status'] == 'success':
+            country = rjson['country']
+            country_code = rjson['countryCode']
+
+    return jsonify({
+        'ip': ip,
+        'country': country,
+        'country_code': country_code
+        }), 200
